e131211a5cff3c319a11a60cba8b940a3107d91b
Omer Katz <omer.drow@gmail.com>
2013-10-04 18:43:44 +0300

    Fixed a typo.

--- tests/unit/fakes/test_fake.py
+++ tests/unit/fakes/test_fake.py
@@ -4,7 +4,7 @@ from nose2.tools import such
 from mockingbird import fake
 
 with such.A('Fake Object') as it:
-    @it.should('raise an TypeError when the configuration does not exist')
+    @it.should('raise a TypeError when the configuration does not exist')
     def test_should_raise_type_error_when_configuration_does_not_exist(case):
         with case.assertRaisesRegexp(TypeError, 'A fake testdouble must have a Configuration class.'):
             fake(object)
