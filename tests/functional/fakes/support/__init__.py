f715c6a7d36aba91b25875dde69bbc894255585b
Omer Katz <omer.drow@gmail.com>
2013-10-06 02:45:24 +0300

    Added spacing in order to conform with PEP8.

--- tests/functional/fakes/support/__init__.py
+++ tests/functional/fakes/support/__init__.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+
 class RealObject(object):
     def was_faked(self):
         return False
+
+    @property
+    def real(self):
+        return True
+
+    @real.setter
+    def real(self, value):
+        pass
