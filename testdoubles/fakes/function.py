01efd3aac943d1f5f6165b0abe63e0f3e7003e04
Omer Katz <omer.drow@gmail.com>
2013-10-09 01:45:59 +0300

    Added an introspection mixin prototype.

--- testdoubles/fakes/function.py
+++ testdoubles/fakes/function.py
@@ -1,15 +1,44 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import inspect
+import sys
 
+python3 = sys.version_info[0] == 3
 
-class FakeFunction(object):
+if python3:
+    class FunctionIntrospectionMixin(object):
+        @property
+        def is_unbound_instance_method(self):
+            try:
+                args = inspect.getargspec(self.live)[0]
+                return args[0] == 'self'
+            except IndexError:
+                return False
+
+        @property
+        def is_instance_method(self):
+            return inspect.ismethod(self.live) or self.is_unbound_instance_method
+else:
+    class FunctionIntrospectionMixin(object):
+        @property
+        def is_unbound_instance_method(self):
+            return inspect.ismethod(self.live) and not self.live.__self__
+
+        @property
+        def is_instance_method(self):
+            return inspect.ismethod(self.live) or self.is_unbound_instance_method
+
+class FakeFunction(object, FunctionIntrospectionMixin):
     def __init__(self, live):
         self._live = live
 
     @property
     def live(self):
         return self._live
+
+    @property
+    def is_unbound_instance_method(self):
+        pass
 
     @property
     def is_instance_method(self):
