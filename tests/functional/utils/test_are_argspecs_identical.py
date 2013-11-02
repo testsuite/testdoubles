3f3f018cf50e9d9e3aee41b9acebd5109e7b2cba
Omer Katz <omer.drow@gmail.com>
2013-11-02 10:50:44 +0200

    Added a functional test that verifies that two callables are not identical if one of them has no positional arguments and the other has a varargs argument.

--- tests/functional/utils/test_are_argspecs_identical.py
+++ tests/functional/utils/test_are_argspecs_identical.py
@@ -22,7 +22,7 @@ with such.A('keyword arguments comparison method') as it:
 
     @it.should(
         'return true if the argspecs are not completely identical but have the same number of positional arguments')
-    def test_should_return_true_if_the_argspecs_are_completely_identical_but_have_the_same_number_of_positional_arguments(case):
+    def test_should_return_true_if_the_argspecs_are_not_completely_identical_but_have_the_same_number_of_positional_arguments(case):
         def fake_callable1(a):
             pass
 
@@ -34,8 +34,8 @@ with such.A('keyword arguments comparison method') as it:
         case.assertTrue(actual)
 
     @it.should(
-        'return true if the argspecs are not completely identical but the first method contains a varargs argument')
-    def test_should_return_true_if_the_argspecs_are_completely_identical_but_the_first_method_contains_a_varargs_argument(case):
+        'return true if the argspecs are not completely identical but the first method has a varargs argument')
+    def test_should_return_true_if_the_argspecs_are_not_completely_identical_but_the_first_method_has_a_varargs_argument(case):
         def fake_callable1(*args):
             pass
 
@@ -47,8 +47,8 @@ with such.A('keyword arguments comparison method') as it:
         case.assertTrue(actual)
 
     @it.should(
-        'return true if the argspecs are not completely identical but the second method contains a varargs argument')
-    def test_should_return_true_if_the_argspecs_are_completely_identical_but_the_second_method_contains_a_varargs_argument(case):
+        'return true if the argspecs are not completely identical but the second method has a varargs argument')
+    def test_should_return_true_if_the_argspecs_are_not_completely_identical_but_the_second_method_has_a_varargs_argument(case):
         def fake_callable1(a):
             pass
 
@@ -58,5 +58,31 @@ with such.A('keyword arguments comparison method') as it:
         actual = are_argspecs_identical(fake_callable1, fake_callable2)
 
         case.assertTrue(actual)
+        
+    @it.should(
+        'return false if the first method has a varargs argument and the second method has no positional arguments')
+    def test_should_return_false_if_the_first_method_has_a_varargs_argument_and_the_second_method_has_no_positional_arguments(case):
+        def fake_callable1(*args):
+            pass
+
+        def fake_callable2(k=mock.DEFAULT):
+            pass
+
+        actual = are_argspecs_identical(fake_callable1, fake_callable2)
+
+        case.assertEqual(actual, False)
+
+    @it.should(
+        'return false if the first method has no positional arguments and the second method has a varargs argument')
+    def test_should_return_false_if_the_first_method_has_no_positional_arguments_and_the_second_method_has_a_varargs_argument(case):
+        def fake_callable1(k=mock.DEFAULT):
+            pass
+
+        def fake_callable2(*args):
+            pass
+
+        actual = are_argspecs_identical(fake_callable1, fake_callable2)
+
+        case.assertEqual(actual, False)
 
     it.createTests(globals())
