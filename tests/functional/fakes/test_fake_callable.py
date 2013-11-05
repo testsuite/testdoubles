923d65edfbb12ddb10fde86ce9fa415deb9b496b
Omer Katz <omer.drow@gmail.com>
2013-11-05 14:08:21 +0200

    Added functional tests for the bound method's internal attributes.

--- tests/functional/fakes/test_fake_callable.py
+++ tests/functional/fakes/test_fake_callable.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from inspect import ArgSpec
+from unittest import skipUnless
 from nose2.tools import such
+import six
 from testdoubles.fakes.callables import FakeCallable
 from tests.common.layers import FunctionalTestsLayer
 from tests.common.compat import mock
@@ -31,6 +34,140 @@ with such.A("Fake Function object") as it:
         actual = sut.__name__
 
         case.assertEqual(actual, expected)
+        
+    @it.should("have a reference to the instance if the method is bound")
+    def test_should_have_a_reference_to_the_instance_if_the_method_is_bound(case):
+        class Foo(object):
+            def bar(self):
+                pass
+
+        live_bound_method = Foo().bar
+        expected = live_bound_method.__self__
+
+        sut = FakeCallable(live_bound_method)
+
+        actual = sut.__self__
+
+        case.assertEqual(actual, expected)
+
+    @it.should("raise an attribute error when accessing __self__ and the method is not a bound or unbound instance method")
+    def test_should_raise_an_attribute_error_when_accessing_self_and_the_method_is_not_a_bound_or_unbound_instance_method(case):
+        def foo():
+            pass
+
+        live_unbound_method = foo
+
+        with case.assertRaisesRegexp(AttributeError,
+                                     r"'function' object has no attribute '__self__'"):
+            sut = FakeCallable(live_unbound_method)
+
+            _ = sut.__self__
+
+    @it.should("raise an attribute error when attempting to use the im_self alias")
+    @skipUnless(six.PY3, 'Test should only be run under Python 3.x')
+    def test_should_raise_an_attribute_error_when_attempting_to_use_the_im_self_alias(case):
+        class Foo(object):
+            def bar(self):
+                pass
+
+        live_unbound_method = Foo.bar
+
+        sut = FakeCallable(live_unbound_method)
+
+        with case.assertRaisesRegexp(AttributeError,
+                                     r"'FakeCallable' object has no attribute 'im_self'"):
+
+            _ = sut.im_self
+
+    @it.should("raise an attribute error when attempting to use the im_class internal attribute")
+    @skipUnless(six.PY3, 'Test should only be run under Python 3.x')
+    def test_should_raise_an_attribute_error_when_attempting_to_use_the_im_class_internal_attribute(case):
+        class Foo(object):
+            def bar(self):
+                pass
+
+        live_unbound_method = Foo.bar
+
+        sut = FakeCallable(live_unbound_method)
+
+        with case.assertRaisesRegexp(AttributeError,
+                                     r"'FakeCallable' object has no attribute 'im_class'"):
+
+            _ = sut.im_class
+
+    @it.should("have an attribute named im_self that is equal to the __self__ attribute")
+    @skipUnless(not six.PY3, 'Test should only be run under Python 2.x')
+    def test_should_have_an_attribute_named_im_self_that_is_equal_to_the_self_attribute(case):
+        class Foo(object):
+            def bar(self):
+                pass
+
+        live_bound_method = Foo().bar
+        expected = live_bound_method.__self__
+
+        sut = FakeCallable(live_bound_method)
+
+        actual = sut.im_self
+
+        case.assertEqual(actual, expected)
+
+    @it.should("have an attribute named im_class that is equal to the __self__ attribute's type")
+    @skipUnless(not six.PY3, 'Test should only be run under Python 2.x')
+    def test_should_have_an_attribute_named_im_self_that_is_equal_to_the_self_attribute_type(case):
+        class Foo(object):
+            def bar(self):
+                pass
+
+        live_bound_method = Foo().bar
+        expected = live_bound_method.__self__.__class__
+
+        sut = FakeCallable(live_bound_method)
+
+        actual = sut.im_class
+
+        case.assertEqual(actual, expected)
+
+    @it.should("have a reference to the fake unbound version of the method if the method is bound")
+    def test_should_have_a_reference_to_the_fake_unbound_version_of_the_method_if_the_method_is_bound(case):
+        class Foo(object):
+            def bar(self):
+                pass
+
+        live_bound_method = Foo().bar
+
+        sut = FakeCallable(live_bound_method)
+        expected = sut.fake.__func__
+        actual = sut.__func__
+
+        case.assertEqual(actual, expected)
+
+    @it.should("not have a reference to the fake unbound version of the method if the method is unbound")
+    def test_should_not_have_a_reference_to_the_fake_unbound_version_of_the_method_if_the_method_is_unbound(case):
+        class Foo(object):
+            def bar(self):
+                pass
+
+        live_unbound_method = Foo.bar
+
+        sut = FakeCallable(live_unbound_method)
+        expected = None
+
+        actual = sut.__func__
+
+        case.assertEqual(actual, expected)
+
+    @it.should("raise an attribute error when accessing __func__ and the method is not a bound or unbound instance method")
+    def test_should_raise_an_attribute_error_when_accessing_func_and_the_method_is_not_a_bound_or_unbound_instance_method(case):
+        def foo():
+            pass
+
+        live_unbound_method = foo
+
+        with case.assertRaisesRegexp(AttributeError,
+                                     r"'function' object has no attribute '__func__'"):
+            sut = FakeCallable(live_unbound_method)
+
+            _ = sut.__func__
 
     it.createTests(globals())
 
