323a342c8749464caebc2a57a0a23e4ac4e92e18
Omer Katz <omer.drow@gmail.com>
2013-11-05 15:30:12 +0200

    Tests will now run under Python 2.6.

--- tests/common/compat.py
+++ tests/common/compat.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import six
+import sys
+
 try:
     from unittest import mock
 except ImportError:
     import mock
 
-try:
-    import unittest
-except ImportError:
-    import unittest2 as unittest
+if not six.PY3 and sys.version_info[1] == 6:
+        import unittest2 as unittest
+else:
+    try:
+        import unittest
+    except ImportError:
+        import unittest2 as unittest
 
 __all__ = ['mock', 'unittest']
